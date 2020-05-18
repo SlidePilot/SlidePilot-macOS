8af46d02399149f057a9493ad5256db9362b9355
Pascal Braband <pascal.braband@web.de>
2020-05-18 21:07:46 +0200

    Fixed arrow key responder chain (arrow keys not working with text field) Removed key equivalents from next/previous slide. Therefore handled next/previous key events in windows. Thus also fixed ThumbnailNavigation with inserting any character to search bar when pressing it.

---
+++ SlidePilot/NavigationWindow.swift
@@ -0,0 +1,25 @@
+//
+//  NavigationWindow.swift
+//  SlidePilot
+//
+//  Created by Pascal Braband on 18.05.20.
+//  Copyright © 2020 Pascal Braband. All rights reserved.
+//
+
+import Cocoa
+
+class NavigationWindow: NSWindow {
+    
+    override func keyDown(with event: NSEvent) {
+        if event.keyCode == 123 || event.keyCode == 126 || event.specialKey == NSEvent.SpecialKey.pageUp {
+            PageController.previousPage(sender: self)
+        } else if event.keyCode == 124 || event.keyCode == 125 || event.specialKey == NSEvent.SpecialKey.pageDown {
+            PageController.nextPage(sender: self)
+        } else {
+            if !self.performKeyEquivalent(with: event) {
+                NSApp.mainMenu?.performKeyEquivalent(with: event)
+            }
+        }
+    }
+
+}
---
+++ SlidePilot/PresenterWindow.swift
@@ -0,0 +1,13 @@
+//
+//  PresenterWindow.swift
+//  SlidePilot
+//
+//  Created by Pascal Braband on 18.05.20.
+//  Copyright © 2020 Pascal Braband. All rights reserved.
+//
+
+import Cocoa
+
+class PresenterWindow: NavigationWindow {
+
+}
