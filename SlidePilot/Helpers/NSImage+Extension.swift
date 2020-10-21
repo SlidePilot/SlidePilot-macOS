a6f0508e05da0220b49705a5d77200f7818b0b6f
Pascal Braband <pascal.braband@web.de>
2020-10-21 19:19:54 +0200

    Implemented sending higher resolution image.

--- SlidePilot/Helpers/NSImage+Extension.swift
+++ SlidePilot/Helpers/NSImage+Extension.swift
@@ -11,11 +11,25 @@ import Cocoa
 extension NSImage {
     
     func compressed() -> Data? {
-        guard let cgImg = self.cgImage(forProposedRect: nil, context: nil, hints: nil) else { return nil }
+        let factor: CGFloat = 3
+        let doubleImageSize = NSSize(width: self.size.width*factor, height: self.size.height*factor)
+        let doubleImage = self.resize(size: doubleImageSize)
+
+        guard let cgImg = doubleImage.cgImage(forProposedRect: nil, context: nil, hints: nil) else { return nil }
         let imgRep = NSBitmapImageRep(cgImage: cgImg)
-        imgRep.size = self.size
+        imgRep.size = doubleImageSize
         guard let data = imgRep.representation(using: .jpeg, properties: [.compressionFactor: 1.0]) else { return nil }
-        print(data.count)
+        
         return data
+    }
+    
+    
+    func resize(size: CGSize) -> NSImage {
+        let newImage = NSImage(size: size)
+        newImage.lockFocus()
+        self.draw(in: NSRect(x: 0, y: 0, width: size.width, height: size.height), from: NSRect(x: 0, y: 0, width: self.size.width, height: self.size.height), operation: .sourceOver, fraction: 1)
+        newImage.unlockFocus()
+        newImage.size = size
+        return newImage
     }
 }
