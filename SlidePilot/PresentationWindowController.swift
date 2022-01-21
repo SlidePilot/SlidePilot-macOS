728df0325e097489d83db1627b26f68eaa6cb1d4
moritzmhmk <moritzmhmk@gmail.com>
2022-01-21 22:33:36 +0100

    presentation window: show/hide titlebar on mouse entered/exited

--- SlidePilot/PresentationWindowController.swift
+++ SlidePilot/PresentationWindowController.swift
@@ -9,11 +9,35 @@
 import Cocoa
 
 class PresentationWindowController: NSWindowController {
+    internal var trackingTag: NSView.TrackingRectTag?
 
+    override func mouseEntered(with event: NSEvent) {
+        if trackingTag == event.trackingNumber {
+            // show titlebar (i.e. superview of closebutton)
+            self.window?.standardWindowButton(.closeButton)?.superview?.animator().alphaValue = 1
+            if #available(OSX 11.0, *) {
+                self.window?.titlebarSeparatorStyle = .shadow
+            }
+        }
+    }
+    
+    override func mouseExited(with event: NSEvent) {
+        if trackingTag == event.trackingNumber {
+            // hide titlebar (i.e. superview of closebutton)
+            self.window?.standardWindowButton(.closeButton)?.superview?.animator().alphaValue = 0
+            if #available(OSX 11.0, *) {
+                self.window?.titlebarSeparatorStyle = .none
+            }
+        }
+    }
+    
     override func windowDidLoad() {
         super.windowDidLoad()
 
         self.window?.title = NSLocalizedString("Presentation", comment: "Window name for the presentation view.")
+        
+        let view = self.window?.contentView
+        trackingTag = view?.addTrackingRect(view!.bounds, owner: self, userData: nil, assumeInside: false)
     }
     
 }
